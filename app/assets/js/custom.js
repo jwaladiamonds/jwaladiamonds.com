ced663a444c59c5bd2d48bbe9fccb567d684ed34
Nikhil John <nikhiljohn1010@gmail.com>
2021-07-29 05:45:57 +0530

    Updated gulp and file structure

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -1,7 +1,27 @@
-a = "hello"
-console.log(a)
-alert(a)
+let posts
+alert("hello")
+document.body.onload = async (e) => {
+    console.log("document loaded!")
+    posts = await fetch("https://www.techiediaries.com/api/feed.json").then(function (response) {
+        return response.json()
+    })
+    posts.forEach(element => {
+        let child = document.createElement('div')
+        child.classList.add('mt-1');
+        ['card'].forEach((v) => {
+            child.classList.add(v)
+        })
+        child.innerHTML = `
+                <hr>
+                <div class="card-body">
+                    <h2 class="card-title">${element.title}</h2>
+                    <p class="card-text">${element.excerpt}</p>
+                    <div class="card-footer">
+                        <a href="${element.url}" class="card-link">Read</a>
+                        <a href="#" class="card-link">Save to read offline</a>
+                    </div>
 
-function say_hello(name) {
-    return "hello" + name
+                </div>`
+        document.getElementById("postsDiv").appendChild(child)
+    })
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,141 @@
-const del = require('del');
-const gulp = require('gulp');
-const rev = require('gulp-rev');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const minify = require('gulp-minify');
-const concat = require('gulp-concat');
-const gulpSass = require('gulp-sass');
-const nodeSass = require('node-sass');
-const cleanCss = require('gulp-clean-css');
-const browserSync = require('browser-sync').create();
+const fs = require('fs')
+const del = require('del')
+const gulp = require('gulp')
+const crypto = require('crypto')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const minify = require('gulp-minify')
+const concat = require('gulp-concat')
+const gulpSass = require('gulp-sass')
+const nodeSass = require('node-sass')
+const htmlparser = require("htmlparser2")
+const cleanCss = require('gulp-clean-css')
+const browserSync = require('browser-sync').create()
 
 const sass = gulpSass(nodeSass)
 
-const paths = {
-    root_dir: './app',
-    index: './index.html',
-    html: 'app/*.html',
-    assets: {
-        css_dir: 'app/assets/css/',
-        css: 'app/assets/css/*.css',
-        js: 'app/assets/js/*.js',
-        sass: 'app/assets/scss/*.scss'
-    },
-    public: {
-        dir: 'app/public/',
-        css_dir: 'app/public/css/',
-        css: 'all.min.css',
-        js_dir: 'app/public/js/',
-        js: 'all.min.js',
-        cache: 'app/public/rev-manifest.json'
-    }
-};
+const sha384 = (buffer) => crypto
+  .createHash('sha384')
+  .update(buffer)
+  .digest('base64')
 
-gulp.task('clean-cache', function () {
-    return del([paths.public.cache]);
-});
+const paths = {
+  root_dir: './app',
+  index: './index.html',
+  html: 'app/*.html',
+  assets: {
+    css_dir: 'app/assets/css/',
+    css: 'app/assets/css/*.css',
+    js: 'app/assets/js/*.js',
+    sass: 'app/assets/scss/*.scss'
+  },
+  public: {
+    dir: 'app/public/',
+    css: 'all.min.css',
+    css_dir: 'app/public/css/',
+    css_file: 'app/public/css/all.min.css',
+    css_html: 'public/css/all.min.css',
+    js: 'all.min.js',
+    js_dir: 'app/public/js/',
+    js_file: 'app/public/js/all.min.js',
+    js_html: 'public/js/all.min.js',
+  }
+}
+
+function hash384 () {
+  const cssBuffer = fs.readFileSync(paths.public.css_file)
+  const jsBuffer = fs.readFileSync(paths.public.js_file)
+  return {
+    css: 'sha384-' + sha384(cssBuffer),
+    js: 'sha384-' + sha384(jsBuffer)
+  }
+}
 
 gulp.task('clean-js', function () {
-    return del([paths.public.js_dir]);
-});
- 
+  return del([paths.public.js_dir])
+})
+
 gulp.task('clean-css', function () {
-    return del([paths.public.css_dir]);
-});
+  return del([paths.public.css_dir])
+})
 
-gulp.task('clean', gulp.series(['clean-cache', 'clean-js', 'clean-css']))
+gulp.task('clean', gulp.series(['clean-js', 'clean-css']))
 
-gulp.task('pack-js', function () {    
-    return gulp
-        .src([paths.assets.js], { sourcemaps: true })
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(concat(paths.public.js))
-        .pipe(minify({
-            ext:{
-                min:'.js'
-            },
-            noSource: true
-        }))
-        .pipe(rev())
-        .pipe(gulp.dest(paths.public.js_dir))
-        .pipe(rev.manifest(paths.public.cache, {
-            merge: true
-        }))
-        .pipe(gulp.dest('.'));
-});
+gulp.task('js', function () {
+  return gulp
+    .src([paths.assets.js])
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat(paths.public.js))
+    .pipe(minify({
+      ext: {
+        min: '.js'
+      },
+      noSource: true
+    }))
+    .pipe(gulp.dest(paths.public.js_dir))
+})
 
-gulp.task('pack-css', function () {    
-    return gulp
-        .src([paths.assets.css])
-        .pipe(concat(paths.public.css))
-        .pipe(cleanCss())
-        .pipe(rev())
-        .pipe(gulp.dest(paths.public.css_dir))
-        .pipe(rev.manifest(paths.public.cache, {
-            merge: true
-        }))
-        .pipe(gulp.dest('.'));
-});
+gulp.task('css', function () {
+  return gulp
+    .src([paths.assets.css])
+    .pipe(concat(paths.public.css))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(paths.public.css_dir))
+})
 
-gulp.task('compile-sass', function() {
-    return gulp
+gulp.task('sass', function () {
+  return gulp
     .src(paths.assets.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.assets.css_dir))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 })
 
-gulp.task('pack', gulp.series(['compile-sass', 'pack-css', 'pack-js']));
+gulp.task('inject', function (done) {
 
-gulp.task('reload', function(done){
-    browserSync.reload();
-    done();
-});
+  const json = hash384()
 
-gulp.task('serve', function(done) {
-    browserSync.init({
-        server: paths.root_dir,
-        index: paths.index,
-    });
-    gulp.watch(paths.assets.js, gulp.series(['pack-js', 'reload']));
-    gulp.watch(paths.assets.css, gulp.series(['pack-css', 'reload']));
-    gulp.watch(paths.assets.sass, gulp.series(['compile-sass', 'reload']));
-    done();
-});
+  const css_tag = /<link href=.+all\.min\.css.+(\n?.*integrity.+\n?.+crossorigin.+)?>/i
+  let new_css = `<link href="${paths.public.css_html}" rel="stylesheet"
+  integrity="${json.css}" crossorigin="anonymous">`
 
-gulp.task('default', gulp.series(['clean', 'pack', 'serve']));
+  const js_tag = /<script src=.+all\.min\.js.+(\n?.*integrity.+\n?.+crossorigin.+)?><\/script>/i
+  let new_js = `<script src="${paths.public.js_html}"
+  integrity="${json.js}" crossorigin="anonymous"></script>`
+
+  fs.readFile('./app/index.html', 'utf8', (err, html) => {
+
+    if (err) throw (err)
+
+    let partialInjected = html.replace(css_tag, new_css)
+    const injected = partialInjected.replace(js_tag, new_js)
+
+    fs.writeFile('./app/index.html', injected, 'utf8', function (err) {
+
+      if (err) throw (err)
+      done()
+
+    })
+  })
+})
+
+gulp.task('reload', function (done) {
+  browserSync.reload()
+  done()
+})
+
+gulp.task('serve', function (done) {
+  browserSync.init({
+    server: paths.root_dir,
+    index: paths.index,
+  })
+  gulp.watch(paths.assets.js, gulp.series(['js', 'inject']))
+  gulp.watch(paths.assets.css, gulp.series(['css', 'inject']))
+  gulp.watch(paths.assets.sass, gulp.series(['sass']))
+  gulp.watch(paths.html, gulp.series(['reload']))
+  done()
+})
+
+gulp.task('pack', gulp.series(['sass', 'css', 'js', 'inject']))
+gulp.task('default', gulp.series(['clean', 'pack', 'serve']))
